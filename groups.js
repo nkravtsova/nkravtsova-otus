f1defe977a1af6d77721adac6c5f06e2d5bf51b9
nkravtsova <nkravtsova@solit-clouds.ru>
2022-04-07 17:27:04 +0400

    lesson1 js

--- groups.js
+++ groups.js
@@ -1,28 +1,28 @@
-    function maxItemAssociation(arr){
-      
-        if (arr){
+function maxItemAssociation(arr){
+  
+	if (arr){
 
-          let groups = new Set();
-          for (let itemArray of arr){
+	  let groups = new Set();
+	  for (let itemArray of arr){
 
-            if (groups.size == 0) {
-              groups.add(new Set(itemArray));
+		if (groups.size == 0) {
+		  groups.add(new Set(itemArray));
 
-            } else {
+		} else {
 
-              for (let itemSet of groups){
-                let addArray = false; 
-                for (let itemGroups of itemSet){
-                  if (itemArray.includes(itemGroups)){
-                    itemArray.forEach(a => itemSet.add(a));
-                    addArray = true;
-                  }
-                }
-                if (!addArray) groups.add(new Set(itemArray));
-              }
-            }
-          }
+		  for (let itemSet of groups){
+			let addArray = false; 
+			for (let itemGroups of itemSet){
+			  if (itemArray.includes(itemGroups)){
+				itemArray.forEach(a => itemSet.add(a));
+				addArray = true;
+			  }
+			}
+			if (!addArray) groups.add(new Set(itemArray));
+		  }
+		}
+	  }
 
-        for (let itemSet of groups) alert(Array.from(itemSet).sort());
-        }
-    }
+	for (let itemSet of groups) alert(Array.from(itemSet).sort());
+	}
+}
--- sum.js
+++ sum.js
@@ -1,14 +1,14 @@
 function sum(value){
-      let currentSum = value;
+  let currentSum = value;
 
-      function cs(currentValue){
-        currentSum +=currentValue;
-        return cs;
-      }
-      
-      cs.toString = function(){
-        return currentSum;
-      }
+  function cs(currentValue){
+	currentSum +=currentValue;
+	return cs;
+  }
+  
+  cs.toString = function(){
+	return currentSum;
+  }
 
-      return cs;
-    }
+  return cs;
+}
